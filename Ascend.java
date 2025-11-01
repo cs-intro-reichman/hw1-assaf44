6ffee6bbde5081f7d45bf473c884b654ef895fe7
ASSAF44 <assaf.barel@post.runi.ac.il>
2025-11-01 23:47:50 +0200

    completed assignment

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,20 @@
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
-public class Ascend {
-	public static void main(String[] args) {
-		// Replace this comment with your code
+public class Ascend  {
+	public static void main(String[] args){
+		// step 1: setting lim according to a given number.
+		int lim = Integer.parseInt(args[0]);
+		// step 2: generating 3 random numbers in the field between 0 and lim.
+		int a = (int) (Math.random() * lim);
+		int b = (int) (Math.random() * lim);
+		int c = (int) (Math.random() * lim);
+		// step 3: organizing the numbers according to their value.
+		int min = Math.min(Math.min(a, b), c);
+		int max = Math.max(Math.max(a, b), c);
+		int mid = (a + b + c) - min - max;
+		// printing the results of step 2.
+		System.out.println(a + " " + b + " "  + c);
+		// printing the results of step 3.
+		System.out.println(min + " " + mid + " " + max);
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,15 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	// step 1: recieving the number.
+	int fullNumber = Integer.parseInt(args[0]);
+	// step 2: finding how many hundreds are in args[0] from step 1.
+	int hundreds = (fullNumber / 100) % 10;
+	// step 3: finding how many tens are in args[0] from step 1.
+	int tens = (fullNumber / 10) % 10;
+	// step 4: finding how many ones are in args[0] from step 1.
+	int units = fullNumber % 10;
+	// step 5: printing steps 2 - 4 + manners as wanted.
+	System.out.println(hundreds + " hundreds " + tens + " tens and " + units + " ones");
 	}
 }
