ef79e669d148dc3893d20c0314611607ae6b79f9
ASSAF44 <assaf.barel@post.runi.ac.il>
2025-11-02 00:52:02 +0200

    completed assignment

--- NumWords.java
+++ NumWords.java
@@ -4,7 +4,7 @@ public class NumWords {
 	// step 1: recieving the number.
 	int fullNumber = Integer.parseInt(args[0]);
 	// step 2: finding how many hundreds are in args[0] from step 1.
-	int hundreds = (fullNumber / 100) % 10;
+	int hundreds = (fullNumber / 100);
 	// step 3: finding how many tens are in args[0] from step 1.
 	int tens = (fullNumber / 10) % 10;
 	// step 4: finding how many ones are in args[0] from step 1.
